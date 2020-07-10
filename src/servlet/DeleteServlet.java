df75ea7bd4774c538a65a0e3dfa27de15acf1b33
SeiichiN <billie175@gmail.com>
2020-07-10 14:37:45 +0900

    DeleteServlet.java added.

---
+++ src/servlet/DeleteServlet.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/DeleteServlet")
+public class DeleteServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet (HttpServletRequest request,
+                          HttpServletResponse response)
+        throws ServletException, IOException {
+
+        System.out.println("DeleteServlet here!");
+        
+        RequestDispatcher dispatcher =
+            request.getRequestDispatcher("/WEB-INF/jsp/delete.jsp");
+        dispatcher.forward( request, response );
+    }
+
+    protected void doPost (HttpServletRequest request,
+                           HttpServletResponse response)
+        throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        String userId = request.getParameter("userId");
+
+        boolean result = DeleteLogic.execute( userId );
+
+        // 成功
+        if (result) {
+            HttpSession session = request.getSession();
+            session.setAttribute( "userId", userId );
+
+            System.out.println("削除成功");
+
+            RequestDispatcher dispatcher =
+                request.getRequestDispatcher("/WEB-INF/jsp/delete.jsp");
+            dispatcher.forward( request, response );
+        }
+        // 登録失敗
+        else {
+            System.out.println("削除失敗");
+            response.sendRedirect("/sukkiriShop/WelcomeServlet");
+        }
+    }
+}
+
+// 修正時刻： Wed Jul  8 20:59:09 2020
